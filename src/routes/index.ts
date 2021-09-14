746b50b5c660718e3124e4bddf52356eb4369b63
rayssa.andrade <rayssa.andrade071@outlook.com>
2021-09-14 20:33:08 -0300

    mapeado rota

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import usersRouter from './users.routes';
 import sessionsRoute from './sessions.routes';
 import measuressRouter from './measuress.routes';
 import datasheetsRouter from './datasheets.routes';
+import macronutrientsRouter from './macronutrients.routes';
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use('/users', usersRouter);
 router.use('/sessions', sessionsRoute);
 router.use('/datasheets', datasheetsRouter);
 router.use('/measuress', measuressRouter);
+router.use('/macronutrients', macronutrientsRouter);
 
 export default router;
