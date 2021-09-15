35a7735012baa293e5143949f63cf8969fe017d1
rayssa.andrade <rayssa.andrade071@outlook.com>
2021-09-15 08:03:49 -0300

    finalizado array de macros

--- src/routes/macronutrients.routes.ts
+++ src/routes/macronutrients.routes.ts
@@ -31,6 +31,13 @@ macronutrientsRoutes.post('/user/', async (request, response)=> {
     });
 
     var array = new Array();
+    var Calorias = {
+        title: "Calorias",
+        body: "As calorias é quantidade de energia necessária para elevar a temperatura de 1 grama de água de 14,5 oC para 15,5 oC.",
+        value: macronutrient.calories,
+        un: 'kcal'
+    }
+    array.push(Calorias);
     var Carboidratos = {
         title: "Carboidratos",
         body: "Os carboidratos evitam que as proteínas dos tecidos sejam utilizadas para o fornecimento de energia.",
@@ -45,6 +52,13 @@ macronutrientsRoutes.post('/user/', async (request, response)=> {
         un: 'g'
     }
     array.push(Proteinas);
+    var Gorduras = {
+        title: "Gorduras",
+        body: "As gorduras protegem os órgãos contra lesões, ajuda a manter a temperatura do corpo, a absorver algumas vitaminas e a dar sensação de saciedade.",
+        value: macronutrient.fats,
+        un: 'g'
+    }
+    array.push(Gorduras);
 
     return response.json(array);
     
