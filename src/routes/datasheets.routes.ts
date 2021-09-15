3cb20ab834ba780e9bcddf71e06cb6b3adac6bf4
rayssa.andrade <rayssa.andrade071@outlook.com>
2021-09-14 22:41:20 -0300

    teste de calcular macros

--- src/routes/datasheets.routes.ts
+++ src/routes/datasheets.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { getRepository } from 'typeorm';
 
 import CreateDatasheetsService from '../services/datasheet/CreateDatasheetService';
+import CreateMacronutrientsService from '../services/macronutrient/CreateMacronutrientService';
 import UpdateDatasheetsService from '../services/datasheet/UpdateDatasheetService';
 
 const datasheetsRoutes = Router();
@@ -11,10 +12,36 @@ datasheetsRoutes.post('/', async (request, response)=> {
     const {userId, sex, birthDate, height, weight, goal} = request.body;
 
     const createDatasheets = new CreateDatasheetsService();
+    const createMacronutrients = new CreateMacronutrientsService();
 
     const datasheet = await createDatasheets.execute({
         userId, sex, birthDate, height, weight, goal
     });
+
+    if (!datasheet){
+
+        var today = new Date();
+        var birthD = new Date(birthDate);
+        var age = today.getFullYear() - birthD.getFullYear();
+        var m = today.getMonth() - birthD.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthD.getDate())) 
+        {
+            age--;
+        }
+        var calories = 1.3 * (66.47 + (13.75 * weight) + (5 * height) - (6.8 * age));
+        var protein = 2 * weight;
+        var fats = 0;
+        if(sex == 1){
+            fats = 1 * weight;
+        } else {
+            fats = 0.8 * weight;
+        }
+        var carbohydrates = calories - protein - fats;
+
+        const macronutrient = await createMacronutrients.execute({
+            userId, calories, protein, carbohydrates, fats
+        });
+    }
 
     return response.json(datasheet);
     
